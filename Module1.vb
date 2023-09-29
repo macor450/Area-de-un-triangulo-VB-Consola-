0883ac708aebfa47f12f614e9e10684bef349e63
Martin Cortez <i20050008@monclova.tecnm.mx>
2023-09-29 07:22:25 -0600

    Initial commit

---
+++ Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("teclea el primer valor")
+        Dim valor1 As Integer = CInt(Console.ReadLine)
+
+        Console.WriteLine("teclea el segundo valor")
+        Dim valor2 As Integer = CInt(Console.ReadLine)
+
+        Console.WriteLine("La respuesta es "& valor1 * valor2 / 2)
+        Console.ReadKey()
+
+
+
+    End Sub
+
+End Module
